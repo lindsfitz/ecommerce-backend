900f379a594904241baa6062f85aeee5f3b80b2e
lindsfitz <lindsaytfitz@gmail.com>
2021-10-28 22:31:58 -0500

    updated routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,61 +5,87 @@ const { Category, Product } = require('../../models');
 
 // find all categories
 // be sure to include its associated Products
-router.get('/', (req, res) => {
-
-  const categoryData = Category.findAll({
-    include: [{ model: Product }]
-  });
-  res.status(200).json(categoryData);
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 
 // find one category by its `id` value
 // be sure to include its associated Products
-router.get('/:id', (req, res) => {
-  const categoryData = Category.findByPk(req.params.id, {
-    include: [{ model: Product }]
-  });
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
 
-  if (!categoryData) {
-    res.status(404).json({ message: 'No category associated with that id.' });
-    return;
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category associated with that id.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.status(200).json(categoryData);
 
 });
 
 
 // create a new category
-router.post('/', (req, res) => {
-  const categoryData = Category.create({
-    category_name: req.body.category_name,
-  });
-  res.status(200).json(categoryData);
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryUpdate = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(categoryUpdate)
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 
 // delete a category by its `id` value
-router.delete('/:id', (req, res) => {
-  const categoryData = Category.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-  if (!categoryData) {
-    res.status(404).json({ message: 'No category found with that id.' });
-    return;
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+    
+  } catch (err) {
+    res.status(500).json(err)
   }
-
-  res.status(200).json(categoryData);
-
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,50 +4,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-  const productData = Product.findAll({
-    include: [{ model: Category }, { model: Tag }]
-  });
-  res.status(200).json(productData);
-
+// find all products
+// be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-  const productData = Product.findByPk(req.params.id, {
-    include: [{ model: Category }, { model: Tag }]
-  });
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
 
-  if (!productData) {
-    res.status(404).json({ message: 'No product associated with that id.' });
-    return;
+    if (!productData) {
+      res.status(404).json({ message: 'No product associated with that id.' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.status(200).json(productData);
 });
 
 // create new product
+/* req.body should look like this...
+  {
+    product_name: "Basketball",
+    price: 200.00,
+    stock: 3,
+    tagIds: [1, 2, 3, 4]
+  }
+*/
 router.post('/', (req, res) => {
 
-  const productData = Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    tagIds: req.body.tagIds
-  });
-  res.status(200).json(productData)
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -112,8 +112,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!productData) {
+      res.status(404).json({message: 'No product found with that id.'})
+      return;
+    }
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
